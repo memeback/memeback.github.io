785f53c3354492b6060d159f72d6363cdf9c4b02
D. Bohdan <dbohdan@dbohdan.com>
2021-02-06 17:35:31 +0000

    feat(gen): improve page layout
    Match the original more closely.

--- src/memeback.css.js
+++ src/memeback.css.js
@@ -1,15 +1,28 @@
+const width = 500;
+const onDesktop = `@media (min-width: ${width}px)`;
+const onMobile = `@media (max-width: ${width - 1}px)`;
+
+const padding = 4;
+
 const css = `
+
 body {
   font-family: Arial, Helvetica, sans-serif;
   font-size: 16px;
-  margin: 0 auto;
-  width: 620px;
+  max-width: ${width + 2*padding}px;
+}
+
+${onDesktop} {
+  body {
+    margin: 8px auto;
+  }
 }
 
 header {
   background: black;
   color: white;
   text-align: center;
+  padding: ${padding}px 0;
 }
 
 header > h1 {
@@ -22,6 +35,7 @@ nav {
   display: flex;
   font-size: 62.5%;
   justify-content: space-between;
+  margin: ${padding * 3}px 0;
   width: 100%;
 }
 
@@ -33,12 +47,21 @@ nav {
   margin-right: 1em;
 }
 
+section > h2 {
+  padding-left: ${padding}px;
+}
+
 article {
-  padding: 4px;
+  padding: ${padding}px;
 }
 
 article:nth-of-type(odd) {
   background: #ddd;
+}
+
+article > .metadata {
+  font-size: 62.5%;
+  padding-top: ${padding}px;
 }
 
 time.date {
@@ -46,13 +69,20 @@ time.date {
 }
 
 footer {
+  padding: ${padding}px 0;
+}
+
+footer > * {
   font-size: 62.5%;
+  padding: ${padding}px;
+  text-align: center;
 }
 
 #copyright {
   background: black;
   color: white;
 }
+
 `;
 
 console.log(css.trim());
